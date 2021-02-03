2d501bdd4452aceb735cb8dceffc8b1f7b96a79d
PilotJinix <agungsantoso557@gmail.com>
2021-02-03 19:46:14 +0700

    Fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigIncrements('id_user');
+            $table->string('email');
+            $table->string('npk')->unique();
+            $table->string('unitkerja');
+            $table->string('nama_user');
+            $table->string('foto_user')->nullable();
+            $table->string('username');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
